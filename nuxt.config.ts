02042eb6f31f8ddb668cc89ce3c9b8c6eb867edb
RyuzakiEX <adanzajorge.dev@gmail.com>
2025-09-09 06:07:17 -0400

    chore: update nuxt-ui to nuxt ui pro

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui-pro'],
 })
