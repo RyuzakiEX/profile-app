8718e97ee5c92373cf308b1f1b312e26de1375b5
RyuzakiEX <adanzajorge.dev@gmail.com>
2025-09-14 02:14:00 -0400

    feat: create a side bar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <UAside class="h-full">
+    <UUser
+      :name="profile.name"
+      :description="profile.description"
+      :avatar="{
+        src: profile.avatar.src,
+        icon: profile.avatar.icon,
+      }"
+    />
+  </UAside>
+</template>
+
+<script setup lang="ts">
+const profile = {
+  name: "Jorge Gabriel Adrian M. Adanza",
+  description: "Software Developer/Engineer",
+  avatar: {
+    src: "/profile.jpeg",
+    icon: "i-lucide-image",
+  },
+};
+</script>
