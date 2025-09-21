2f919bcd4876d0514d4f350a855343de65a5f828
RyuzakiEX <adanzajorge.dev@gmail.com>
2025-09-21 12:38:56 -0400

    feat: add temporary layout, sidebar and navbar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,14 +1,23 @@
 <template>
-  <UAside class="h-full">
+  <UPageAside class="h-full flex justify-center">
     <UUser
+      class="mt-2 align-center text-center"
+      orientation="vertical"
       :name="profile.name"
       :description="profile.description"
-      :avatar="{
-        src: profile.avatar.src,
-        icon: profile.avatar.icon,
-      }"
-    />
-  </UAside>
+    >
+      <template #avatar>
+        <div class="flex justify-center">
+          <UAvatar
+            class="profile-avatar"
+            :src="profile.avatar.src"
+            :icon="profile.avatar.icon"
+          />
+        </div>
+      </template>
+    </UUser>
+  </UPageAside class="h-full flex justify-center">
+>
 </template>
 
 <script setup lang="ts">
@@ -21,3 +30,12 @@ const profile = {
   },
 };
 </script>
+
+<style scoped>
+.profile-avatar {
+  width: 110px;
+  height: 110px;
+  border-radius: 9999px;
+  object-fit: cover;
+}
+</style>
