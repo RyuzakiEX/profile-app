186f5a371d8bc9f73af1579f14fea9b717779f4c
RyuzakiEX <adanzajorge.dev@gmail.com>
2025-10-22 03:48:47 -0400

    fix: incorrect brace in config

--- app/app.config.ts
+++ app/app.config.ts
@@ -4,8 +4,8 @@ export default defineAppConfig({
       primary: "purple", // Or any Tailwind color like 'blue', 'indigo', etc.
       gray: "cool", // Or any Tailwind gray variant like 'slate', 'neutral', etc.
     },
-  },
-  container: {
-    base: "w-full max-w-(--ui-container) mx-auto px-4 sm:px-6 lg:px-8 !py-5",
+    container: {
+      base: "w-full max-w-(--ui-container) mx-auto px-4 sm:px-6 lg:px-8 !py-5",
+    },
   },
 });
