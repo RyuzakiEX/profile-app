307624b198345515d7719be7f9683d8c13960643
RyuzakiEX <adanzajorge.dev@gmail.com>
2025-10-22 03:11:42 -0400

    style: change theme to purple

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -28,16 +28,46 @@
           pause-on-hover
           :ui="{ root: '[--gap:--spacing(1)]' }"
         >
-          <UIcon name="i-simple-icons-python" class="size-10 shrink-0" />
-          <UIcon name="i-simple-icons-nuxtdotjs" class="size-10 shrink-0" />
-          <UIcon name="i-simple-icons-vuedotjs" class="size-10 shrink-0" />
-          <UIcon name="i-simple-icons-databricks" class="size-10 shrink-0" />
-          <UIcon name="i-simple-icons-tensorflow" class="size-10 shrink-0" />
-          <UIcon name="i-simple-icons-cplusplus" class="size-10 shrink-0" />
-          <UIcon name="i-simple-icons-javascript" class="size-10 shrink-0" />
-          <UIcon name="i-simple-icons-typescript" class="size-10 shrink-0" />
-          <UIcon name="i-simple-icons-roboflow" class="size-10 shrink-0" />
-          <UIcon name="i-simple-icons-ultralytics" class="size-10 shrink-0" />
+          <UIcon
+            name="i-simple-icons-python"
+            class="size-10 shrink-0 bg-purple-600"
+          />
+          <UIcon
+            name="i-simple-icons-nuxtdotjs"
+            class="size-10 shrink-0 bg-purple-600"
+          />
+          <UIcon
+            name="i-simple-icons-vuedotjs"
+            class="size-10 shrink-0 bg-purple-600"
+          />
+          <UIcon
+            name="i-simple-icons-databricks"
+            class="size-10 shrink-0 bg-purple-600"
+          />
+          <UIcon
+            name="i-simple-icons-tensorflow"
+            class="size-10 shrink-0 bg-purple-600"
+          />
+          <UIcon
+            name="i-simple-icons-cplusplus"
+            class="size-10 shrink-0 bg-purple-600"
+          />
+          <UIcon
+            name="i-simple-icons-javascript"
+            class="size-10 shrink-0 bg-purple-600"
+          />
+          <UIcon
+            name="i-simple-icons-typescript"
+            class="size-10 shrink-0 bg-purple-600"
+          />
+          <UIcon
+            name="i-simple-icons-roboflow"
+            class="size-10 shrink-0 bg-purple-600"
+          />
+          <UIcon
+            name="i-simple-icons-ultralytics"
+            class="size-10 shrink-0 bg-purple-600"
+          />
         </UMarquee>
       </template>
 
