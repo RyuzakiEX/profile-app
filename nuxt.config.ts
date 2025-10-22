37dab313082b284fc9ff8492357c83487ab08599
RyuzakiEX <adanzajorge.dev@gmail.com>
2025-10-21 23:54:41 -0400

    chore: update nuxt ui from v3.3 to v4 and remove unused component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/ui-pro"],
+  modules: ["@nuxt/ui", "@nuxt/image"],
 });
