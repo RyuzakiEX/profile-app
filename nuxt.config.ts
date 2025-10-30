97749e8005e15046b0117187975bb913b1b97187
RyuzakiEX <adanzajorge.dev@gmail.com>
2025-10-30 11:50:51 -0400

    chore: add i18n for translation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxt/ui", "@nuxt/image", "@nuxthub/core"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxt/image",
+    "@nuxthub/core",
+    "@nuxtjs/i18n",
+  ],
   ui: {},
 });
