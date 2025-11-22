fbbc5a2b56a1012b028dec7a4ec54777af01855c
RyuzakiEX <adanzajorge.dev@gmail.com>
2025-11-22 21:32:26 +0800

    chore: remove nodemailer from config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,21 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  modules: [
-    "@nuxt/ui",
-    "@nuxt/image",
-    "@nuxthub/core",
-    "@nuxtjs/i18n",
-    "nuxt-nodemailer",
-  ],
-  nodemailer: {
-    from: process.env.NUXT_NODEMAILER_FROM,
-    host: process.env.NUXT_NODEMAILER_HOST,
-    port: Number(process.env.NUXT_NODEMAILER_PORT),
-    secure: true,
-    auth: {
-      user: process.env.NUXT_NODEMAILER_AUTH_USER,
-      pass: process.env.NUXT_NODEMAILER_AUTH_PASS,
-    },
-  },
+  modules: ["@nuxt/ui", "@nuxt/image", "@nuxthub/core", "@nuxtjs/i18n"],
 });
