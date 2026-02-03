4746ee0b829aad16ef0a6b10cf1f4bec6d28ea11
RyuzakiEX <adanzajorge.dev@gmail.com>
2026-02-03 22:58:24 +0800

    feat: increase modal component width

--- app/app.config.ts
+++ app/app.config.ts
@@ -7,5 +7,10 @@ export default defineAppConfig({
     container: {
       base: "w-full max-w-(--ui-container) mx-auto px-4 sm:px-6 lg:px-8 !py-5",
     },
+    modal: {
+      slots: {
+        content: "w-[90vw] min-w-5xl",
+      },
+    },
   },
 });
